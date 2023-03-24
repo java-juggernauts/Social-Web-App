03b9b963a575c797f7a440bafcdcdde38f6782c4
Stephen <stephenasupe@gmail.com>
2023-03-24 15:39:15 -0500

    styling

--- src/components/chatroom/Message.js
+++ src/components/chatroom/Message.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { auth } from "lib/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-
+import "./message.css";
 const Message = ({ message, senderUsername, senderAvatar }) => {
   const [user] = useAuthState(auth);
 
